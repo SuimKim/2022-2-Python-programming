c3e477e607685ea4c5905dfbf144921504e7e1f2
김수임 | Suim Kim <94280082+SuimKim@users.noreply.github.com>
2022-03-08 15:48:19 +0900

    3월 8일  수업 내용 업로드

---
+++ cal02.py
@@ -0,0 +1,43 @@
+#반드시 함수 정의 한 후 함수 호출 
+#더하기 함수 정의 
+def add(a, b):
+        c = a+b
+        return c
+#빼기 함수 정의 
+def minus(a,b):
+    c=a-b
+    return c
+#곱하기 함수 정의 
+def multi(a,b):
+    c=a*b
+    return c
+#나누기 함수 정의 
+def divide(a,b):
+    c=a/b
+    return c
+    
+while True:
+    a = input("1st input?")
+    if a=="0000":
+        break
+    
+    a = int(a)
+    b = int(input("2nd input?"))
+
+    #더하기 함수 호출 
+    result = add(a, b)
+    print(a, "+", b, "=", result)
+
+    #빼기 함수 호출
+    result = minus(a, b)
+    print(a, "-", b, "=", result)
+
+    #곱하기 함수 호출
+    result = multi(a, b)
+    print(a, "*", b, "=", result)
+
+    #나누기 함수 호출
+    result = divide(a, b)
+    print(a, "/", b, "=", result)
+    
+print("exit")
