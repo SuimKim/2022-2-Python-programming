8735480d0d6bd4c7bb221492eded5927100cf99c
김수임 | Suim Kim <94280082+SuimKim@users.noreply.github.com>
2022-03-15 15:33:55 +0900

    turtle 원 만들기

---
+++ Ch02/turtle_circle.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+t.shape("turtle")
+t.pensize(4)
+
+t.color("blue") #line
+t.bgcolor("yellow") #background
+t.fillcolor("red") #채우기
+
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+
+t.done()
