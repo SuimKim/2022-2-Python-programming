00eb09074d339b85d8071e883852eee3efde6e7f
김수임 | Suim Kim <94280082+SuimKim@users.noreply.github.com>
2022-03-15 15:33:17 +0900

    파이썬 ch01-연산, 줄바꿈

---
+++ Ch01/test2.py
@@ -0,0 +1,14 @@
+# 1~10 sum
+'''
+sum=0
+for i in range(2, 11, 2):
+    sum += i
+    print(i, "+", sum-i, "=", sum)
+
+print("total = ", sum)
+'''
+
+data = '안녕 ' +\
+       '하세요? ' +\
+       '파이썬! '
+print(data)
